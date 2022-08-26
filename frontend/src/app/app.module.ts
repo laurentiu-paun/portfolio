3a650a4842f339119ef4f75f7d96caae339cdedf
laurentiu-paun <105289179+laurentiu-paun@users.noreply.github.com>
2022-08-26 15:45:09 +0300

    Styles done until drop-down (including custom arrow))

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./routes.module";
-import {MenuComponent} from "./components/menu/menu.component";
-import {FooterComponent} from "./components/footer/footer.component";
-
-const Components = [
-  MenuComponent,
-  FooterComponent
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Components
+    AppComponent
   ],
   imports: [
     BrowserModule,
