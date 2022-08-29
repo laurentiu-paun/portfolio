a9e6b1da6aac4115e7cd282a5e4dfb10739f6d98
laurentiu-paun <105289179+laurentiu-paun@users.noreply.github.com>
2022-08-29 11:55:07 +0300

    Responsive table done

--- frontend/src/app/routes.module.ts
+++ frontend/src/app/routes.module.ts
@@ -5,16 +5,18 @@ import {ContactComponent} from "./pages/contact/contact.component";
 import {CvComponent} from "./pages/cv/cv.component";
 import {WorkComponent} from "./pages/work/work.component";
 import {ResponsiveTableComponent} from "./pages/projects/responsive-table/responsive-table.component";
+import {ColorPaletteComponent} from "./pages/projects/color-palette/color-palette.component"
 import {NgModule} from "@angular/core";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'cv', component: CvComponent},
-  {path: 'work', component: WorkComponent},
-  {path: 'ResponsiveTable', component: ResponsiveTableComponent},
-  { path: '**', redirectTo: 'home' }
+  {path: 'home', component: HomeComponent, title: "Frontend - Home"},
+  {path: 'about', component: AboutComponent, title: "Frontend - Despre mine"},
+  {path: 'contact', component: ContactComponent, title: "Frontend - Contact"},
+  {path: 'cv', component: CvComponent, title: "Frontend - CV"},
+  {path: 'work', component: WorkComponent, title: "Frontend - Proiecte"},
+  {path: 'ResponsiveTable', component: ResponsiveTableComponent, title: "Proiecte - Tabel Responsive"},
+  {path: 'ColorPalette', component: ColorPaletteComponent, title: "Proiecte - Paleta de culori"},
+  { path: '**', redirectTo: 'home' ,title: "Frontend"}
 ]
 
 @NgModule({
@@ -25,4 +27,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule {
 }
-export const routingComponents = [HomeComponent, AboutComponent, ContactComponent, CvComponent, WorkComponent, ResponsiveTableComponent]
+export const routingComponents = [HomeComponent, AboutComponent, ContactComponent, CvComponent, WorkComponent, ResponsiveTableComponent, ColorPaletteComponent]
